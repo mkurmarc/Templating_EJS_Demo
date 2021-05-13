1467bf4092a5204e1db8fdbfd9a76b2306adc1d5
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-05-12 21:45:21 -0700

    passing data from js file to ejs file

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+// allows us to use EJS
 app.set('view engine', 'ejs');
+
+// this joins the path name of this file index.js with '/views';
+// allows for running index.js outside of working directory. 
+app.set('views', path.join(__dirname, '/views'));
+
+
 
 app.get('/', (req, res) => {
     res.render('home');  // aka 'views/home.ejs'
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { rand: num }); // the render method's 2nd arg accepts object with key-value pairs;
+                                        // this object is pushed thru to html and accesible there with key.
 })
 
 app.listen(3000, () => {
